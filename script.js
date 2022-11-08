931d2dc4edd7bbb6de11d1af1bb7f54868ca1749
OilPainter1 <hjcryns@gmail.com>
2022-11-08 09:19:04 -0600

    added generateHTML.js, added questions to inquirer prompt

---
+++ script.js
@@ -0,0 +1,26 @@
+const inquirer  = require("inquirer");
+
+const questions1 = [{
+    name: "teamManagerName",
+    type: "input",
+    message: "Please enter the Team Manager's name:"
+},
+{
+    name: "teamManagerID",
+    type: "input",
+    message: "Please enter the Team Manager's employee ID:"
+
+},
+{
+    name: "teamManagerEmail",
+    type: "input",
+    message: "Please enter the Team Manager's email adress:"
+},
+{
+    name: "teamManagerOfficeNumber",
+    type: "input",
+    message: "Please enter the Team Manager's office number:"
+}
+]
+
+inquirer.prompt(questions1).then(answers => console.log(answers))
