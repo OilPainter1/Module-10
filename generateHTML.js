2c730fd1e94b6f6b0c51965f6bb84644d9d4c026
OilPainter1 <hjcryns@gmail.com>
2022-11-08 09:21:50 -0600

    added exports and imports to javascript files

---
+++ generateHTML.js
@@ -0,0 +1,6 @@
+function generateHTML(
+
+)
+
+
+module.exports(generateHTML)
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const inquirer  = require("inquirer");
+const generateHTML = require("./generateHTML")
 
 const questions1 = [{
     name: "teamManagerName",
