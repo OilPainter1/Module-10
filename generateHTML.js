2d56119656b11da71ae25c2c80947e056adb3a99
OilPainter1 <hjcryns@gmail.com>
2022-11-09 11:09:44 -0600

    completed generateCardManager function in generateHTML.js

--- generateHTML.js
+++ generateHTML.js
@@ -24,11 +24,10 @@ function generateCardManager (name,ID,email,officeNumber){
     `
     <div class="card" style="width: 18rem;">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
+            <h5 class="card-title">${name} (Team Manager)</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Employee ID: ${ID}</h6>
+            <h6 class="card-subtitle mb-2 text-muted">Office Number: ${officeNumber}</h6>
+            <a href="mailto: ${email}" class="card-link">${email}</a>
         </div>
     </div>
     `
