b87f684687e8fc270302efbbf4f1ca82d96d24d1
OilPainter1 <hjcryns@gmail.com>
2022-11-09 10:24:53 -0600

    added generateCard functions to generateHTML javascript file

--- generateHTML.js
+++ generateHTML.js
@@ -6,7 +6,7 @@ export default function generateHTML(data){
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
         <title>Document</title>
     </head>
     <body>
@@ -19,5 +19,36 @@ export default function generateHTML(data){
     return string
 }
 
+function generateCardManager (name,ID,email,officeNumber){
+    var cardHTML = 
+    `
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+        </div>
+    </div>
+    `
+    return cardHTML
+}
+
+function generateCard (name,ID,email,officeNumber,gitHub){
+    var cardHTML = 
+    `
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+        </div>
+    </div>
+    `
+    return cardHTML
+}
 
 
